59be702cbccbe9be2036ae93d537a9d78018c0a6
AntiBoomz <AntiBoomz@users.noreply.github.com>
2017-02-01 20:41:22 +0200

    Adds context menu support for new catalog/library urls

--- js/background/contextmenu.js
+++ js/background/contextmenu.js
@@ -13,19 +13,21 @@ function copyToClipboard(text) {
 }
 
 var itemUrlPatterns = [
-	"*://www.roblox.com/*-item?id=*"
+	"*://www.roblox.com/*-item?id=*",
+	"*://www.roblox.com/catalog/*"
+	"*://www.roblox.com/library/*"
 ]
 
 chrome.contextMenus.create({
 	title: "Copy asset id",
 	contexts: ["link"],
 	targetUrlPatterns: itemUrlPatterns,
-	onclick: (info, tab) => copyToClipboard(info.linkUrl.replace(/^.*[&?]id=(\d+).*$/, "$1"))
+	onclick: (info, tab) => copyToClipboard(info.linkUrl.replace(/^.*(?:[&?]id=|\/(?:catalog|library)\/)(\d+).*$/, "$1"))
 })
 
 chrome.contextMenus.create({
 	title: "Copy asset id",
 	contexts: ["page"],
 	documentUrlPatterns: itemUrlPatterns,
-	onclick: (info, tab) => copyToClipboard(info.pageUrl.replace(/^.*[&?]id=(\d+).*$/, "$1"))
+	onclick: (info, tab) => copyToClipboard(info.pageUrl.replace(/^.*(?:[&?]id=|\/(?:catalog|library)\/)(\d+).*$/, "$1"))
 })
