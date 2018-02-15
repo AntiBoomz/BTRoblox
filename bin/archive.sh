8b4a1e57e8130efa924db740cd93319d7c731be8
AntiBoomz <AntiBoomz@users.noreply.github.com>
2018-02-15 19:58:23 +0200

    Rename manifest.chrome.json to manifest.json

--- bin/archive.sh
+++ bin/archive.sh
@@ -4,8 +4,5 @@ git archive --format=zip -9 -o bin/roblox.chrome.zip HEAD
 cd bin
 cp roblox.chrome.zip roblox.firefox.zip
 
-7z a roblox.chrome.zip ../manifest.chrome.json
-7z rn roblox.chrome.zip manifest.chrome.json manifest.json
-
 7z a roblox.firefox.zip ../manifest.firefox.json
 7z rn roblox.firefox.zip manifest.firefox.json manifest.json
