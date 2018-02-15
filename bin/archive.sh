fe5d93b9141f74ab07f81c0e0237afbf1f5f2893
AntiBoomz <AntiBoomz@users.noreply.github.com>
2018-02-15 21:41:50 +0200

    Archive.sh: Delete duplicate manifest for firefox

--- bin/archive.sh
+++ bin/archive.sh
@@ -4,5 +4,6 @@ git archive --format=zip -9 -o bin/roblox.chrome.zip HEAD
 cd bin
 cp roblox.chrome.zip roblox.firefox.zip
 
+7z d roblox.firefox.zip manifest.json
 7z a roblox.firefox.zip ../manifest.firefox.json
 7z rn roblox.firefox.zip manifest.firefox.json manifest.json
