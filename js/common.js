cbb2ca8aeed3cfebf335eb487e60051547c3e392
AntiBoomz <AntiBoomz@users.noreply.github.com>
2018-11-12 22:54:49 +0200

    Switch hover preview default back to always

--- js/common.js
+++ js/common.js
@@ -387,7 +387,7 @@ const SETTINGS = {
 			robuxToDollars: { default: true, value: false },
 			robuxToDollarsRate: { default: true, value: "devex350", validValues: ["devex350", "devex250", "nbc5", "nbc10", "nbc25", "nbc50", "nbc100", "nbc200", "bc5", "bc10", "bc25", "bc50", "bc100", "bc200"] },
 			hoverPreview: { default: true, value: true },
-			hoverPreviewMode: { default: true, value: "never", validValues: ["always", "never"] }
+			hoverPreviewMode: { default: true, value: "always", validValues: ["always", "never"] }
 		},
 		navigation: {
 			enabled: { default: true, value: true },
