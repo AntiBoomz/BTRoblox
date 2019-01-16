8ad1bc007419cea2c4b3c420e1e95c7bdc395da4
AntiBoomz <AntiBoomz@users.noreply.github.com>
2019-01-16 09:51:03 +0200

    Shell 'tis

--- bin/archive.bat
+++
@@ -1,9 +0,0 @@
-cd ..
-git archive --format=zip -9 -o bin/btroblox.chrome.zip HEAD
-
-cd bin
-cp btroblox.chrome.zip btroblox.firefox.zip
-
-7z d btroblox.firefox.zip manifest.json
-7z a btroblox.firefox.zip ../manifest.firefox.json
-7z rn btroblox.firefox.zip manifest.firefox.json manifest.json
