4519ca92a22abf041a2c0822ef7092a4dadad984
AntiBoomz <AntiBoomz@users.noreply.github.com>
2019-12-21 15:07:44 +0200

    Adding browserAction on onInstalled doesn't work

--- js/background/browseraction.js
+++ js/background/browseraction.js
@@ -1,21 +1,19 @@
 "use strict"
 
 {
-	chrome.runtime.onInstalled.addListener(() => {
-		chrome.browserAction.onClicked.addListener(tab => {
-			if(tab.url) {
-				chrome.tabs.executeScript(
-					tab.id,
-					{ code: `(() => { if(typeof ToggleSettingsDiv === "function") { ToggleSettingsDiv(true); return true } })()`, runAt: "document_start" },
-					([result]) => {
-						if(!result) {
-							chrome.tabs.create({ url: "https://www.roblox.com/home?btr_settings_open=true" })
-						}
+	chrome.browserAction.onClicked.addListener(tab => {
+		if(tab.url) {
+			chrome.tabs.executeScript(
+				tab.id,
+				{ code: `(() => { if(typeof ToggleSettingsDiv === "function") { ToggleSettingsDiv(true); return true } })()`, runAt: "document_start" },
+				([result]) => {
+					if(!result) {
+						chrome.tabs.create({ url: "https://www.roblox.com/home?btr_settings_open=true" })
 					}
-				)
-			} else {
-				chrome.tabs.create({ url: "https://www.roblox.com/home?btr_settings_open=true" })
-			}
-		})
+				}
+			)
+		} else {
+			chrome.tabs.create({ url: "https://www.roblox.com/home?btr_settings_open=true" })
+		}
 	})
 }
