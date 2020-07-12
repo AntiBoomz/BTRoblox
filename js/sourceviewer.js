061651e1528f985156e8ba832229b080917da222
AntiBoomz <AntiBoomz@users.noreply.github.com>
2020-07-12 04:26:41 +0300

    Update sourceviewer syntax highlighter

--- js/sourceviewer.js
+++ js/sourceviewer.js
@@ -1,8 +1,19 @@
 
 "use strict"
 
-const ParseRegex = /\w+|[^\n\S]+|(?:--(?:\[\[([^\]]*|(?!\]\])\])*\]\]|\[=\[([^\]]*|(?!\]=\])\])*\]=\]|\[==\[([^\]]*|(?!\]==\])\])*\]==\]|[^\n]*))|(?:\[\[([^\]]*|(?!\]\])\])*\]\]|\[=\[([^\]]*|(?!\]=\])\])*\]=\]|\[==\[([^\]]*|(?!\]==\])\])*\]==\]|"(?:[^"\\]*|\\.)*"|'(?:[^'\\]*|\\.)*')|(?:(?:-?(?:\d*\.?\d+|0x[0-9a-fA-F]+)(?:[eE]-?\d+)?)|\W)|./yg
 const NumberRegex = /^-?(?:\d*\.?\d+|0x[0-9a-fA-F]+)(?:[eE]-?\d+)?$/
+const ParseRegex = new RegExp(
+	[
+		/-?(?:\d*\.?\d+|0x[0-9a-fA-F]+)(?:[eE]-?\d+)?/.source, // number
+		/[+\-*/^%~=><]=/.source, // multi-char ops
+		/\[=*\[/.source, // groups
+		/--(?:\[=*\[)?/.source, // comments
+		/\w+/.source, // words
+		/[^\n\S]+/.source, // whitespace
+		/[^]/.source // any character
+	].join("|"),
+	"yg"
+)
 
 const Keywords = new Set([
 	"function", "true", "false", "local", "end", "if", "elseif", "else", "then", "repeat", "until", "while", "do",
@@ -12,8 +23,8 @@ const Keywords = new Set([
 const Globals = new Set([
 	"Axes", "BrickColor", "CellId", "CFrame", "Color3", "ColorSequence", "ColorSequenceKeypoint", "DateTime",
 	"DockWidgetPluginGuiInfo", "Faces", "Instance", "NumberRange", "NumberSequence", "NumberSequenceKeypoint",
-	"PathWaypoint", "PhysicalProperties", "PluginDrag", "Random", "Ray", "Rect", "Region3", "Region3int16",
-	"TweenInfo", "UDim", "UDim2", "Vector2", "Vector2int16", "Vector3", "Vector3int16",
+	"PathWaypoint", "PhysicalProperties", "PluginDrag", "Random", "Ray", "RaycastParams", "Rect", "Region3",
+	"Region3int16", "TweenInfo", "UDim", "UDim2", "Vector2", "Vector2int16", "Vector3", "Vector3int16",
 
 	"assert", "collectgarbage", "error", "gcinfo", "getfenv", "getmetatable", "ipairs", "loadstring", "newproxy",
 	"next", "pairs", "pcall", "print", "rawequal", "rawget", "rawset", "select", "setfenv", "setmetatable",
@@ -30,16 +41,21 @@ const Tables = {
 	coroutine: new Set(["resume", "yield", "running", "status", "wrap", "create", "isyieldable"]),
 	debug: new Set(["traceback", "profileend", "profilebegin"]),
 	math: new Set([
-		"log", "ldexp", "rad", "cosh", "random", "frexp", "tanh", "floor", "max", "sqrt", "modf", "huge", "pow", "atan", "tan", "cos", "sign",
-		"clamp", "log10", "noise", "acos", "abs", "pi", "sinh", "asin", "min", "deg", "fmod", "randomseed", "atan2", "ceil", "sin", "exp"
+		"abs", "acos", "asin", "atan", "atan2", "ceil", "clamp", "cos", "cosh", "deg", "exp", "floor", "fmod",
+		"frexp", "huge", "ldexp", "log", "log10", "max", "min", "modf", "noise", "pi", "pow", "rad", "random",
+		"randomseed", "sign", "sin", "sinh", "sqrt", "tan", "tanh"
 	]),
-	os: new Set(["difftime", "time", "date"]),
+	os: new Set(["difftime", "time", "date", "clock"]),
 	string: new Set(["sub", "split", "upper", "len", "find", "match", "char", "rep", "gmatch", "reverse", "byte", "format", "gsub", "lower"]),
 	table: new Set(["pack", "move", "insert", "getn", "foreachi", "foreach", "concat", "unpack", "find", "create", "sort", "remove"]),
 	utf8: new Set(["offset", "codepoint", "nfdnormalize", "char", "codes", "len", "graphemes", "nfcnormalize", "charpattern"])
 }
 
-const Operators = new Set(["+", "-", "*", "/", "%", "=", "==", "~=", ">", ">=", "<", "<=", ":", "{", "}", "(", ")", "[", "]", "#"])
+const Operators = new Set([
+	"+", "-", "*", "/", "%", ">", "<", "=",
+	"+=", "-=", "*=", "/=", "%=", ">=", "<=", "==", "~=",
+	":", "{", "}", "(", ")", "[", "]", "#"
+])
 
 const ScopeIn = new Set(["then", "do", "repeat", "function", "(", "{", "["])
 const ScopeOut = new Set(["end", "elseif", "until", ")", "}", "]"])
@@ -52,6 +68,7 @@ function parseSource(source) {
 	let lineN = 0
 	let depth = 0
 	let current
+	let text
 
 	const content = html`
 	<div class=content>
@@ -80,29 +97,97 @@ function parseSource(source) {
 	// Add an empty line to the beginning
 	content.append(html`<div class=line-container><div class=linenumber></div><div class=line style=height:5px;><span class=scope></span><span class=linetext></span></div></div>`)
 
+	const appendUntil = final => {
+		const len = final - ParseRegex.lastIndex
+
+		if(len > 0) {
+			text += source.slice(ParseRegex.lastIndex, ParseRegex.lastIndex + len)
+			ParseRegex.lastIndex += len
+		}
+	}
+
+	const appendGroup = groupDepth => {
+		const searchString = `]${ "=".repeat(groupDepth) }]`
+		const groupEndIndex = source.indexOf(searchString)
+
+		if(groupEndIndex === -1) {
+			appendUntil(source.length)
+		} else {
+			appendUntil(groupEndIndex + searchString.length)
+		}
+	}
+
+	const genericAppend = stringChar => {
+		let newLine = source.indexOf("\n", ParseRegex.lastIndex)
+		if(newLine === -1) { newLine = source.length }
+		
+		if(stringChar) {
+			let nextIndex = ParseRegex.lastIndex
+
+			while(true) {
+				nextIndex = source.indexOf(stringChar, nextIndex)
+
+				if(nextIndex === -1 || nextIndex > newLine) {
+					appendUntil(newLine)
+					return
+				}
+
+				let pointer = nextIndex
+				while(source[--pointer] === "\\") {
+					// do nothing
+				}
+
+				if((nextIndex - pointer) % 2 === 1) {
+					appendUntil(nextIndex + 1)
+					return
+				}
+			}
+		} else {
+			appendUntil(newLine)
+		}
+	}
+
 	current = createLine()
+	ParseRegex.lastIndex = 0
+	
 	while(true) {
 		const match = ParseRegex.exec(source)
+		console.log(match)
 		if(!match) {
 			break
 		}
 
-		const text = match[0]
-
+		text = match[0]
 		if(text === "\n") {
 			current = createLine()
 			continue
 		}
 
-		const textLines = text.split("\n")
+		let textType = "text"
 
-		let textType = text.startsWith("--") ? "comment" :
-			Keywords.has(text) ? "keyword" :
-				Globals.has(text) ? "global" :
-					Operators.has(text) ? "operator" :
-						text.length > 1 && (text[0] === "\"" || text[0] === "'" || text[0] === "[") ? "string" :
-							NumberRegex.test(text) ? "number" :
-								"text"
+		if(text[0] === "-" && text[1] === "-") {
+			textType = "comment"
+
+			if(text.length > 2) {
+				appendGroup(text.length - 4)
+			} else {
+				genericAppend()
+			}
+		} else if(text.length > 1 && text[0] === "[") {
+			textType = "string"
+			appendGroup(text.length - 2)
+		} else if(text === "\"" || text === "'") {
+			textType = "string"
+			genericAppend(text)
+		} else if(Keywords.has(text)) {
+			textType = "keyword"
+		} else if(Globals.has(text)) {
+			textType = "global"
+		} else if(Operators.has(text)) {
+			textType = "operator"
+		} else if(NumberRegex.test(text)) {
+			textType = "number"
+		}
 
 		if(ScopeIn.has(text)) {
 			depth++
@@ -140,7 +225,9 @@ function parseSource(source) {
 			}
 		}
 
+		const textLines = text.split("\n")
 		const multiline = (textType === "comment" || textType === "string") && textLines.length > 1
+
 		textLines.forEach((line, index) => {
 			if(index > 0) {
 				current = createLine()
