c706a0362924231c096a7534a8c9052f14865890
AntiBoomz <AntiBoomz@users.noreply.github.com>
2020-12-30 02:30:45 +0200

    Add advanced accessory button next to redraw

--- js/pages/avatar.js
+++ js/pages/avatar.js
@@ -1,3 +1,12 @@
 "use strict"
 
-pageInit.avatar = () => {}
+pageInit.avatar = () => {
+	if(!SETTINGS.get("avatar.enabled")) {
+		return
+	}
+	
+	modifyTemplate("avatar-base", template => {
+		console.log(template)
+		template.$find(".redraw-avatar .text-link").after(html`<a class="text-link" ng-click="openAdvancedAccessories()" style="margin-right:6px">Advanced</a>`)
+	})
+}
