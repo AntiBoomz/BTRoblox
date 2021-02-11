fcf330eb31bc7bc5ed9a0e3d6e210d6136e3801b
AntiBoomz <AntiBoomz@users.noreply.github.com>
2021-02-11 17:08:37 +0200

    Remove unnecessary console.log

--- js/pages/avatar.js
+++ js/pages/avatar.js
@@ -6,7 +6,6 @@ pageInit.avatar = () => {
 	}
 	
 	modifyTemplate("avatar-base", template => {
-		console.log(template)
 		template.$find(".redraw-avatar .text-link").after(html`<a class="text-link" ng-click="openAdvancedAccessories()" style="margin-right:6px">Advanced</a>`)
 	})
 }
