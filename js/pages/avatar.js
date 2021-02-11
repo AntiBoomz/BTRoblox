6958c1682d9de0ebbd67b5144d445bce78d0c12c
AntiBoomz <AntiBoomz@users.noreply.github.com>
2021-02-11 18:40:57 +0200

    Increase advanced button margin

--- js/pages/avatar.js
+++ js/pages/avatar.js
@@ -6,6 +6,6 @@ pageInit.avatar = () => {
 	}
 	
 	modifyTemplate("avatar-base", template => {
-		template.$find(".redraw-avatar .text-link").after(html`<a class="text-link" ng-click="openAdvancedAccessories()" style="margin-right:6px">Advanced</a>`)
+		template.$find(".redraw-avatar .text-link").after(html`<a class="text-link" ng-click="openAdvancedAccessories()" style="margin-right:10px">Advanced</a>`)
 	})
 }
