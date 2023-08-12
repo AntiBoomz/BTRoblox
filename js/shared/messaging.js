143eacdc3e2302e06d8c047532a60c00721b99e2
AntiBoomz <AntiBoomz@users.noreply.github.com>
2023-08-12 10:44:40 +0300

    chore: cleanup messaging code

--- js/shared/messaging.js
+++ js/shared/messaging.js
@@ -1,118 +1,139 @@
 "use strict"
 
-const MESSAGING = (() => {
-	if(IS_BACKGROUND_PAGE) {
-		const listenersByName = {}
-		const ports = []
+let MESSAGING
 
-		chrome.runtime.onConnect.addListener(port => {
-			let alive = true
-			ports.push(port)
+if(IS_BACKGROUND_PAGE) {
+	MESSAGING = {
+		listenersByName: [],
+		ports: [],
+		
+		onPortAdded(port) {
+			this.ports.push(port)
+	
+			port.onMessage.addListener(msg => this.onPortMessage(port, msg))
+			port.onDisconnect.addListener(() => this.onPortRemoved(port))
+		},
+		
+		onPortRemoved(port) {
+			const index = this.ports.indexOf(port)
+			if(index !== -1) { this.ports.splice(index, 1) }
+		},
+	
+		onPortMessage(port, msg) {
+			const listener = this.listenersByName[msg.name]
 
-			port.onMessage.addListener(msg => {
-				const listener = listenersByName[msg.name]
+			if(!listener) {
+				throw new Error(`Received unknown message ${msg.name}`)
+			}
 
-				if(!listener) {
-					throw new Error(`Received unknown message ${msg.name}`)
+			let final = false
+
+			const respond = (response, hasMore) => {
+				if(!final && "id" in msg) {
+					final = !(hasMore === true)
+
+					port.postMessage({
+						id: msg.id,
+						data: response,
+						final
+					})
 				}
+			}
 
-				let final = false
-
-				const respond = (response, hasMore) => {
-					if(alive && !final && "id" in msg) {
-						final = !(hasMore === true)
-
-						port.postMessage({
-							id: msg.id,
-							data: response,
-							final
-						})
-					}
+			respond.cancel = () => {
+				if(!final && "id" in msg) {
+					final = true
+					port.postMessage({ id: msg.id, final, cancel: true })
 				}
+			}
 
-				respond.cancel = () => {
-					if(alive && !final && "id" in msg) {
-						final = true
-						port.postMessage({ id: msg.id, final, cancel: true })
-					}
+			listener(msg.data, respond, port)
+		},
+		
+		listen(name, callback) {
+			if(typeof name === "object") {
+				for(const [key, fn] of Object.entries(name)) {
+					this.listen(key, fn)
 				}
-
-				listener(msg.data, respond, port)
-			})
-
-			port.onDisconnect.addListener(() => {
-				alive = false
-				const index = ports.indexOf(port)
-				if(index !== -1) { ports.splice(index, 1) }
-			})
-		})
-
-		return {
-			ports,
-
-			listen(name, callback) {
-				if(typeof name === "object") {
-					Object.entries(name).forEach(([key, fn]) => this.listen(key, fn))
-					return
-				}
-
-				if(!listenersByName[name]) {
-					listenersByName[name] = callback
-				} else {
-					console.warn(`Listener '${name}' already exists`)
-				}
+				return
+			}
+	
+			if(!this.listenersByName[name]) {
+				this.listenersByName[name] = callback
+			} else {
+				console.warn(`Listener '${name}' already exists`)
 			}
 		}
 	}
-
-	return {
+	
+	chrome.runtime.onConnect.addListener(port => MESSAGING.onPortAdded(port))
+} else {
+	MESSAGING = {
 		callbacks: {},
 		responseCounter: 0,
+		
+		resetTimeout() {
+			if(this.portTimeout) {
+				clearTimeout(this.portTimeout)
+				this.portTimeout = null
+			}
+			
+			if(this.port && Object.keys(this.callbacks).length === 0) {
+				this.portTimeout = setTimeout(() => this.disconnectPort(), 10e3)
+			}
+		},
+		
+		initPort() {
+			if(this.port) { return }
+			
+			const port = chrome.runtime.connect()
+			this.port = port
+			
+			port.onMessage.addListener(msg => this.onPortMessage(port, msg))
+			port.onDisconnect.addListener(() => this.disconnectPort())
+			
+			this.resetTimeout()
+		},
+		
+		disconnectPort() {
+			if(!this.port) { return }
+			this.port.disconnect()
+			this.port = null
+			
+			this.callbacks = {}
+			this.resetTimeout()
+		},
+		
+		onPortMessage(port, msg) {
+			const fn = this.callbacks[msg.id]
+			if(!fn) { return }
 
+			if(msg.final) {
+				delete this.callbacks[msg.id]
+				this.resetTimeout()
+				
+				if(msg.cancel) { return }
+			}
+
+			fn(msg.data)
+		},
+		
 		send(name, data, callback) {
 			if(typeof data === "function") {
 				callback = data
 				data = null
 			}
-
-			if(!this.port) {
-				const port = this.port = chrome.runtime.connect()
-
-				const doDisconnect = () => {
-					clearTimeout(this.portTimeout)
-					port.disconnect()
-					this.port = null
-				}
-
-				port.onMessage.addListener(msg => {
-					const fn = this.callbacks[msg.id]
-					if(!fn) { return }
-
-					if(msg.final) {
-						delete this.callbacks[msg.id]
-						if(Object.keys(this.callbacks).length === 0) {
-							this.portTimeout = setTimeout(doDisconnect, 10e3)
-						}
-
-						if(msg.cancel) { return }
-					}
-
-					fn(msg.data)
-				})
-
-				port.onDisconnect.addListener(doDisconnect)
-				this.portTimeout = setTimeout(doDisconnect, 10e3)
-			}
-
+			
 			const info = { name, data }
-
+			
 			if(typeof callback === "function") {
 				const id = info.id = this.responseCounter++
 				this.callbacks[id] = callback
-				clearTimeout(this.portTimeout)
 			}
-
+			
+			if(!this.port) { this.initPort() }
 			this.port.postMessage(info)
+			this.resetTimeout()
 		}
 	}
-})()
+}
