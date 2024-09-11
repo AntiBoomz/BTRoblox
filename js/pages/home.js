feffcdbf86f240fd4e29af83f65cba21531df81c
AntiBoomz <AntiBoomz@users.noreply.github.com>
2024-09-12 01:12:28 +0300

    fix: add home page react friends support

--- js/pages/home.js
+++ js/pages/home.js
@@ -2,78 +2,81 @@
 
 pageInit.home = () => {
 	if(SETTINGS.get("home.friendsShowUsername")) {
-		angularHook.modifyTemplate("people", card => {
-			const container = card.$find(".friend-parent-container")
-			
-			if(container) {
-				document.body.classList.add("btr-home-showUsername")
-				container.after(html`<div class="text-overflow xsmall text-label btr-people-username" title="@{{friend.name}}">@{{friend.name}}</div>`)
-			}
-		})
-	}
-	
-	if(SETTINGS.get("home.friendsSecondRow")) {
-		angularHook.modifyTemplate("people-list-container", template => {
-			template.$find(".people-list-container").setAttribute("ng-class", `{"btr-home-secondRow": btrShowSecondRow}`)
-		})
-		
-		angularHook.modifyTemplate("people-list", template => {
-			template.$find(".hlist").setAttribute("style", "--btr-width: {{btrWidth * 128}}px;")
-		})
+		document.$watch(">body", body => body.classList.add("btr-home-showUsername"))
 		
 		InjectJS.inject(() => {
-			const { angularHook } = window.BTRoblox
+			const { reactHook, contentScript } = BTRoblox
 			
-			angularHook.hijackModule("peopleList", {
-				peopleListContainerController(handler, args, argsMap) {
-					const result = handler.apply(this, args)
+			const friendsState = reactHook.createGlobalState({})
+			
+			contentScript.listen("updateFriends", friends => {
+				friendsState.set(friends)
+			})
+			
+			reactHook.hijackConstructor(
+				(type, props) => props.displayName && props.userProfileUrl,
+				(target, thisArg, args) => {
+					const result = target.apply(thisArg, args)
 					
 					try {
-						const { $scope } = argsMap
+						const userId = args[0].id
 						
-						$scope.btrShowSecondRow = false
-						
-						try { $scope.btrShowSecondRow = !!localStorage.getItem("BTRoblox:homeShowSecondRow") }
-						catch(ex) { console.error(ex) }
-						
-						$scope.$watch("library.numOfFriends", numOfFriends => {
-							if(numOfFriends == null) { return }
+						const labels = reactHook.queryElement(result, x => x.props.className?.includes("friends-carousel-tile-labels"))
+						if(labels && Array.isArray(labels.props.children)) {
+							const friends = reactHook.useGlobalState(friendsState)
+							const friend = friends[userId]
 							
-							$scope.btrShowSecondRow = numOfFriends > ($scope.layout.maxNumberOfFriendsDisplayed / 2)
-							$scope.btrWidth = Math.ceil(Math.min($scope.layout.maxNumberOfFriendsDisplayed, numOfFriends) / 2)
-							
-							if($scope.btrShowSecondRow) {
-								localStorage.setItem("BTRoblox:homeShowSecondRow", "true")
-							} else {
-								localStorage.removeItem("BTRoblox:homeShowSecondRow")
+							if(friend) {
+								labels.props.children.splice(1, 0, reactHook.createElement("span", {
+									className: "btr-friends-carousel-real-username",
+									children: `@${friend.name}`
+								}))
 							}
-						})
+						}
 					} catch(ex) {
 						console.error(ex)
 					}
 					
 					return result
-				},
-				layoutService(handler, args) {
-					const result = handler.apply(this, args)
-					result.maxNumberOfFriendsDisplayed *= 2
-					return result
 				}
-			})
+			)
 		})
+		
+		InjectJS.send("updateFriends", btrFriends.getFriends())
+		btrFriends.loadFriends(friends => InjectJS.send("updateFriends", friend))
+	}
+	
+	if(SETTINGS.get("home.friendsSecondRow")) {
+		document.$watch(">body", body => body.classList.add("btr-home-secondRow"))
 	}
 	
 	if(SETTINGS.get("home.friendPresenceLinks")) {
-		angularHook.modifyTemplate("people-info-card", template => {
-			for(const elem of template.$findAll(`[ng-click^="goToGameDetails"]`)) {
-				const anchor = document.createElement("a")
-				anchor.href = `{{friend.presence.placeUrl}}`
-				anchor.append(...elem.childNodes)
-				for(const attr of elem.attributes) {
-					anchor.setAttribute(attr.name, attr.value)
+		InjectJS.inject(() => {
+			const { reactHook } = BTRoblox
+		
+			reactHook.hijackConstructor(
+				(type, props) => props.friend && props.gameUrl,
+				(target, thisArg, args) => {
+					const result = target.apply(thisArg, args)
+					
+					try {
+						const card = result.props.children?.[0]
+						
+						if(card?.props.className?.includes("in-game-friend-card")) {
+							result.props.children[0] = reactHook.createElement("a", {
+								href: args[0].gameUrl,
+								style: { display: "contents" },
+								onClick: event => event.preventDefault(),
+								children: card
+							})
+						}
+					} catch(ex) {
+						console.error(ex)
+					}
+					
+					return result
 				}
-				elem.replaceWith(anchor)
-			}
+			)
 		})
 	}
 }
