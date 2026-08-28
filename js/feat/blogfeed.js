4476f576cf65ece34fae45693727e5ec91cdd455
AntiBoomz <AntiBoomz@users.noreply.github.com>
2026-08-28 19:57:42 +0300

    fix: fix blog feed

--- js/feat/blogfeed.js
+++ js/feat/blogfeed.js
@@ -14,24 +14,21 @@ const BlogFeed = {
 		if(!this.fetching && this.canRequest()) {
 			this.lastRequest = Date.now()
 
-			const feedUrl = `https://api.buttercms.com/v2/pages/long_form_page/?locale=en&preview=0&page=1&page_size=3&fields.page_type.localized_slug=newsroom&fields.unlist_page=false&order=-displayed_publish_date&auth_token=137ac5a15935fab769262b6167858b427157ee3d`
-
-			this.fetching = fetch(feedUrl).then(async res => {
+			this.fetching = fetch(`https://about.roblox.com/search-index/en.json`).then(async res => {
 				const json = await res.json()
 				const posts = []
 				
-				for(let i = 0; i < 3; i++) {
-					const post = json.data[i]
-					if(!post) { break }
-					
-					const published = new Date(post.fields.displayed_publish_date)
+				for(const entry of json) {
+					if(entry.page_type !== "newsroom") { continue }
 					
 					posts.push({
-						url: `https://corp.roblox.com/newsroom/${published.getUTCFullYear()}/${("0" + (published.getUTCMonth() + 1)).slice(-2)}/${post.slug}`,
-						date: post.fields.displayed_publish_date,
-						title: post.fields.title,
-						desc: post.fields.long_form_content?.find(x => x.type === "long-form-text")?.fields.body ?? ""
+						url: entry.url,
+						date: entry.publishDate,
+						title: entry.title,
+						desc: entry.description
 					})
+					
+					if(posts.length >= 3) { break }
 				}
 				
 				this.cached = posts
